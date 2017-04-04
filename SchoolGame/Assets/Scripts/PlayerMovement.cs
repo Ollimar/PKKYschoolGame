8d6249df813510366fe66c944cff98ee348b70c2
Ollimar <ollimar84@gmail.com>
2017-04-04 20:47:39 +0300

    Material Update
    Added TestMaterial1 and TesMaterial2. Applied them to assets in
    TestLevel

--- SchoolGame/Assets/Scripts/PlayerMovement.cs
+++ SchoolGame/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
+    public float speed = 10f;
+    public float jumpVelocity = 10f;
+
+    public bool isGrounded = false;
+
+    public Transform circlePos;
+    public float circleRadius = 1f;
+    public LayerMask whatIsGround;
+
+    private Transform myTransform;
+    private Rigidbody2D myRB; 
+
+
 	void Start () {
-		
+        myRB = GetComponent<Rigidbody2D>();
+        myTransform = this.transform;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+    void Update() {
+        if(Input.GetButtonDown("Jump") && isGrounded) {
+            Jump();
+        }
+    }
+
+	void FixedUpdate () 
+    {
+        Move( Input.GetAxisRaw( "Horizontal" ) );
+        isGrounded = Physics2D.OverlapCircle(circlePos.position,circleRadius,whatIsGround);
 	}
+
+    public void  Move(float horizontalInput) 
+    {
+        Vector2 moveVelocity = myRB.velocity;
+        moveVelocity.x = horizontalInput * speed;
+        myRB.velocity = moveVelocity;
+    }
+
+    public void Jump() {
+
+        if(isGrounded) 
+        {
+            myRB.velocity += jumpVelocity * Vector2.up;
+        }
+    }
 }
