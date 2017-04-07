77a7ca04ab5d2fdd5b1d8c6ddc330c9049e5f79c
Samuel <hietalahti.samuel@gmail.com>
2017-04-07 08:50:13 +0300

    älä välitä, gitin vaatima commitointi

---
+++ SchoolGame/Assets/CharacterCustomization.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterCustomization : MonoBehaviour {
+
+    public Image fadeScreen;
+
+    private Animator anim;
+    private Animator animFadeScreen;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        animFadeScreen = fadeScreen.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RotateRight() 
+    {
+        transform.Rotate( Vector3.up * 10f * Time.deltaTime );
+    }
+
+    public void Done() {
+        anim.SetBool("Done",true);
+    }
+
+    public void FadeScreen() {
+        animFadeScreen.SetBool("Done",true);
+    }
+
+    public void NextLevel() {
+        Application.LoadLevel("TestLevel");
+    }
+}
---
+++ SchoolGame/Assets/Scripts/DoorMetalliala.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMetalliala : MonoBehaviour {
+
+    public Transform text;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "Player") {
+            text.GetComponent<Animator>().SetBool( "AtDoor", true );
+        }
+    }
+
+        void OnTriggerExit2D(Collider2D other) {
+        if(other.gameObject.tag == "Player") {
+            text.GetComponent<Animator>().SetBool( "AtDoor", false );
+        }
+    }
+
+}
