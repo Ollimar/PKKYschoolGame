7f5b54039c18193d8d86fb78b06fad8230faa3cc
Samuel <hietalahti.samuel@gmail.com>
2017-04-08 13:17:06 +0300

    Testing2

---
+++ SchoolGame/Assets/Scripts/ScriptWorldMap.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptWorldMap : MonoBehaviour 
+{
+
+    Vector3 hitPosition = Vector3.zero;
+    Vector3 currentPosition = Vector3.zero;
+    Vector3 cameraPosition = Vector3.zero;
+
+    public Transform pointer;
+    public float pointerTimer = 1f;
+    public float pointerSpeed = 1f;
+    public bool pointerToggle = false;
+
+    public Canvas metallialaImage;
+    public Animator MetallialaAnim;
+
+    public float zoomSpeed = .5f;
+    public float minZoom = 11f;
+    public float maxZoom = 4f;
+
+	void Start () 
+    {
+        metallialaImage.enabled = false;
+        MetallialaAnim = GameObject.Find( "MetallialaImage" ).GetComponent<Animator>();
+	}
+	
+	void Update () 
+     {
+
+        Ray ray;
+        RaycastHit hit;
+
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity)) 
+        {
+            if ( hit.transform.tag == "Metalliala" ) 
+            {
+                if(Input.GetMouseButtonDown(0)) 
+                {
+                    metallialaImage.enabled = true;
+                    MetallialaAnim.SetBool( "Touched", true );
+                }
+            }
+
+         if ( hit.transform.tag == "ground" ) 
+            {
+                if(Input.GetMouseButtonDown(0)) 
+                {
+                    hitPosition = Input.mousePosition;
+                    cameraPosition = new Vector3(transform.position.x,Camera.main.transform.position.y,transform.position.z);
+                    print( "Touch" );
+                }
+
+                if(Input.GetMouseButton(0)) 
+                {
+                    currentPosition = Input.mousePosition;
+                    Drag();
+                    print( "Touch" );
+                }
+            }
+        }
+
+        //Camera Zoom
+
+        if(Input.touchCount == 2) 
+        {
+            Touch touchZero = Input.GetTouch( 0 );
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = ( touchZero.position - touchOne.position ).magnitude;
+
+            float deltaMagnitudediff = prevTouchDeltaMag - touchDeltaMag;
+
+            Camera.main.orthographicSize += deltaMagnitudediff * zoomSpeed;
+            Camera.main.orthographicSize = Mathf.Min( Camera.main.orthographicSize, minZoom );
+            Camera.main.orthographicSize = Mathf.Max( Camera.main.orthographicSize, maxZoom );
+        }
+
+        // Map pointer code
+
+        pointerTimer -= Time.deltaTime;
+
+        if(!pointerToggle) 
+        {
+            pointerSpeed = 1f;
+        }
+
+        if(pointerToggle)  
+        {
+            pointerSpeed = -1f;
+        }
+
+        if(pointerTimer <= 0f) 
+        {
+            pointerToggle = !pointerToggle;
+            pointerTimer = 0.5f;
+        }
+
+        pointer.Translate(Vector3.up*pointerSpeed*Time.deltaTime);
+	}
+
+    void Drag() {
+        Vector3 direction = Camera.main.ScreenToWorldPoint(currentPosition) - Camera.main.ScreenToWorldPoint(hitPosition);
+        direction = direction * -1;
+        Vector3 position = cameraPosition + direction;
+        transform.position = new Vector3(position.x,Camera.main.transform.position.y,position.z);
+    }
+
+    public void closeMetalliala() 
+    {
+           metallialaImage.enabled = false;
+           MetallialaAnim.SetBool( "Touched", false );
+    }
+
+    public void EnterMetalliala() 
+    {
+        Application.LoadLevel("TestLevel");
+    }
+}
